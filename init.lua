1b7c9b89663e912015d8aa97a793a22eee838bbd
Stephen <stephenbrady365@gmail.com>
2025-07-24 16:15:10 +0100

    add folding settings

--- init.lua
+++ init.lua
@@ -625,6 +625,7 @@ require('lazy').setup({
         -- gopls = {},
         pyright = {},
         cssls = {},
+        bashls = {},
         -- rust_analyzer = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
